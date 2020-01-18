123283ecf958527dfb0e0d35736b8433bf650be4
権藤　俊 <gondo.syun0711@gmail.com>
2020-01-18 21:44:10 +0900

    Initial Commit

---
+++ RealTimeFaceTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeFaceTracking
+//
+//  Created by しゅん on 2020/01/18.
+//  Copyright © 2020 g-chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
